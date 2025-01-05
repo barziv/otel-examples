544da01e69254753524bdd2be60c7f16ddc5b6c3
Bar Ziv <barziv@Bars-MacBook-Air.local>
2025-01-05 23:41:57 +0200

    setup elastic for traces

--- nest-otel/src/app.controller.ts
+++ nest-otel/src/app.controller.ts
@@ -16,4 +16,13 @@ export class AppController {
     this.logger.info('im in the controller', {});
     return await this.appService.getHello("bar");
   }
+
+  @Get('number')
+  @Span()
+  async getNumber(): Promise<string> {
+    this.logger.info('im in the number controller', {});
+    const response = await fetch('http://localhost:3000');
+    const text = await response.text();
+    return text;
+  }
 }
